ea035b0c8e86e7a89c6926c397df2a6bbbd722b1
Moses Karani <karanim594@gmail.com>
2024-07-17 07:07:16 +0300

    role call build

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+
+
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "/favicon.ico",
+  });
+});
+
+self.addEventListener("notificationclick", () => {
+  let payload = e.notification.data;
+  clients.openWindow(payload?.["url"]);
+});
