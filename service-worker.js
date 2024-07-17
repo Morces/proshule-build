4276d0694d7c8c7835a5c36f8a6299efae8002e3
Moses Karani <karanim594@gmail.com>
2024-07-17 10:08:40 +0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-=======
-// src/service-worker.js
->>>>>>> 4cbc024de0199b3644e8de03e671cda4d50cc1cd
 
 self.addEventListener("push", (event) => {
   const data = event.data.json();
@@ -11,11 +6,10 @@ self.addEventListener("push", (event) => {
     icon: "/favicon.ico",
   });
 });
-<<<<<<< HEAD
+
 
 self.addEventListener("notificationclick", () => {
   let payload = e.notification.data;
   clients.openWindow(payload?.["url"]);
 });
-=======
->>>>>>> 4cbc024de0199b3644e8de03e671cda4d50cc1cd
+
